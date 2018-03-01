36c747d7e2debef1cac224f378cfd7bfe3a007b3
Yamile Tabares <yamile08.lytm@gmail.com>
2018-03-01 14:13:13 -0500

    Creando una advertencia sobre el borrado de base de datos

--- platziverse-db/setup.js
+++ platziverse-db/setup.js
@@ -1,27 +1,44 @@
 'use strict'
 
 const debug = require('debug')('platziverse:db:setup')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 const db = require('./')
 
+const prompt = inquirer.createPromptModule() //advertencia sobre el borrado de base de datos
+
 async function setup () {
+  const answer = await prompt([  //advertencia sobre el borrado de base de datos
+
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will destroy your database, are you sure?' //advertencia sobre el borrado de base de datos
+    }
+  ])
+
+  if (!answer.setup) {
+    return console.log('Nothing happened :)')
+  }
+
   const config = {
-    database: process.env.DB_NAME || 'platziverse', 
-    username: process.env.DB_USER || 'platzi', //le puedo pasar un usuario como variable de entorno o platzi que fue el usuario que creamos
+    database: process.env.DB_NAME || 'platziverse',
+    username: process.env.DB_USER || 'platzi',
     password: process.env.DB_PASS || 'platzi',
     host: process.env.DB_HOST || 'localhost',
-    dialect: 'postgres', //dialecto de base de datos es postgres, si el dia de mañana se emigra a otra base de datos es solo cambiar el dialect y sequalize lo entendera
+    dialect: 'postgres',
     logging: s => debug(s),
     setup: true
   }
 
-  await db(config).catch(handleFatalError) //En caso de error, nos mostrara el error
+  await db(config).catch(handleFatalError)
 
   console.log('Success!')
   process.exit(0)
 }
 
-function handleFatalError (err) { //En caso de error, nos mostrara el error
-  console.error(err.message)
+function handleFatalError (err) {
+  console.error(`${chalk.red('[fatal error]')} ${err.message}`) //chalk.red('fata error') en caso de error nos muestra en la consola el mensaje en rojo
   console.error(err.stack)
   process.exit(1)
 }
